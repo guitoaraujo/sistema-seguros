1ee116320dad276ba0fa5cfff3683687ba873c12
Guito Araujo <guitoaraujo@gmail.com>
2018-04-10 09:30:17 -0300

    fix migrations

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,7 +2,7 @@ module ClientsHelper
 
   def validity_alert_control(validity)
     class_name = 'alert alert-success'
-    date_diff = (validity - Date.today).to_i
+    date_diff = (validity - Time.now).to_i / 1.day
     if date_diff <= 45
       class_name = 'alert alert-danger'
     elsif date_diff <= 60 && date_diff > 45
